3a37e5c9db61aa8463cc45269d4695fcbbea4813
Nick-NCSU <nathomp3@ncsu.edu>
2021-12-02 00:44:44 -0500

    Added 2020 01

--- 2020/01/part2.c
+++ 2020/01/part2.c
@@ -11,11 +11,13 @@ int main() {
     printf("%d\n", count);
     for(int i = 0; i < count - 1; i++) {
         for(int j = i + 1; j < count; j++) {
-            if(nums[i] + nums[j] == 2020) {
-                printf("%d\n", nums[i] * nums[j]);
-                return 0;
-            }
-        }
+            for(int k = j + 1; k < count; k++) {
+		if(nums[i] + nums[j] + nums[k] == 2020) {
+		        printf("%d\n", nums[i] * nums[j] * nums[k]);
+		        return 0;
+		}
+	     }
+	}
     }
     return 0;
 }
