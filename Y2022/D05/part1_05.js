ab5a17a1e376630822b335d4a53a271de7761b8d
nathomp3 <nathomp3@ncsu.edu>
2022-12-05 00:23:39 -0500

    Added D02 and D05

---
+++ Y2022/D05/part1_05.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+const stacks = [
+  ['Z', 'J', 'N', 'W', 'P', 'S'],
+  ['G', 'S', 'T'],
+  ['V', 'Q', 'R', 'L', 'H'],
+  ['V', 'S', 'T', 'D'],
+  ['Q', 'Z', 'T', 'D', 'B', 'M', 'J'],
+  ['M', 'W', 'T', 'J', 'D', 'C', 'Z', 'L'],
+  ['L', 'P', 'M', 'W', 'G', 'T', 'J'],
+  ['N', 'G', 'M', 'T', 'B', 'F', 'Q', 'H'],
+  ['R', 'D', 'G', 'C', 'P', 'B', 'Q', 'W']
+]
+
+const input = fs.readFileSync('data.txt', { encoding: 'utf-8' }).split(/\r\n\r\n/)[1];
+
+for(const line of input.split('\n')) {
+  const [_line, num, from, to] = line.match(/move (\d+) from (\d+) to (\d+)/)
+
+  for(let i = 0; i < num; i++) {
+    stacks[to - 1].push(stacks[from - 1].pop());
+  }
+}
+
+for(const stack of stacks) {
+  process.stdout.write(stack[stack.length - 1])
+}
