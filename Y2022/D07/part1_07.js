e75409bf2d5399abcf4a2fb4193ecb30a80623b8
nathomp3 <nathomp3@ncsu.edu>
2022-12-07 00:55:18 -0500

    Added D07

---
+++ Y2022/D07/part1_07.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+
+const input = fs.readFileSync('data.txt', { encoding: 'utf-8' }).split('\r\n');
+let nodes = {
+  '~': []
+};
+let dirs = [];
+for(let i = 0; i < input.length; i++) {
+  if(input[i] === '$ ls') {
+    let j = i + 1;
+    while(j < input.length && !input[j].startsWith('$')) {
+      if(!nodes[dirs.join('/')]) {
+        nodes[dirs.join('/')] = [];
+      }
+      const [info, name] = input[j].split(' ');
+      nodes[dirs.join('/')].push({
+        info,
+        name: dirs.join('/') + '/' + name
+      });
+      j++;
+    }
+    i = j - 1;
+  } else if(input[i] === '$ cd ..') {
+    dirs.pop();
+  } else if(input[i] === '$ cd /') {
+    dirs = ['~'];
+  } else {
+    dirs.push(input[i].split(' ')[2]);
+  }
+}
+const totals = {}
+let unfound = Object.keys(nodes);
+while(unfound.length) {
+  const found = []
+  for(const key of unfound) {
+    if(nodes[key].filter(node => node.info === 'dir').length === 0) {
+      totals[key] = nodes[key].reduce((total, val) => total + +val.info, 0);
+      for(const node in nodes) {
+        for(const k of nodes[node]) {
+          if(k.name === key) {
+            k.info = totals[key];
+          }
+        }
+      }
+      found.push(key)
+    }
+  }
+  unfound = unfound.filter(k => !found.includes(k));
+}
+let a = 0;
+for(const node in nodes) {
+  if(nodes[node].reduce((total, val) => total + +val.info, 0) <= 100000) {
+    a +=  nodes[node].reduce((total, val) => total + +val.info, 0);
+  }
+}
+console.log(a)
