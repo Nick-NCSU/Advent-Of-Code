38b668d70f8ed0a2a71ccd898425e8eec48387c9
Nick-ncsu <nathomp3@alumni.ncsu.edu>
2023-12-06 00:10:41 -0500

    Add 2023 D06

---
+++ Y2023/D06/part2.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+const input = fs.readFileSync(`${__dirname}/data.txt`, 'utf8').split('\n')
+
+const times = input[0].replaceAll(/[ ]+/g, '').split(':').slice(1).map(Number);
+const distances = input[1].replaceAll(/[ ]+/g, '').split(':').slice(1).map(Number);
+
+let ans = 1;
+for(let i = 0; i < times.length; i++) {
+  let total = 0;
+  for(let j = 0; j < times[i]; j++) {
+    const speed = j;
+    const score = (times[i] - speed) * speed;
+    if(score > distances[i]) {
+      total++;
+    }
+  }
+  ans *= total;
+}
+console.log(ans);
